bf19c380b684e7cc455fe5d0df53c3f29d814dcc
Jed Billman <billman377@gmail.com>
2019-02-26 09:34:04 -0700

    Added buttons and fixed Gcalendar connection

--- oauth.js
+++ oauth.js
@@ -1,7 +1,7 @@
 window.onload = function() {
     document.querySelector('button').addEventListener('click', function() {
-        chrome.identity.getAuthToken({interactive: true}, function(token) {
-            console.log(token);
-        });
+      chrome.identity.getAuthToken({interactive: true}, function(token) {
+        console.log(token);
+      });
     });
-};
+  };
