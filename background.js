2584d50df096a7ae8901d9e933f122fee5721bda
Jed Billman <billman377@gmail.com>
2019-04-02 09:32:46 -0600

    Added Course Storage

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-
 let _userBrightspace = null;
 let _userCanvas = null;
 let _userGoogle = null;
@@ -12,12 +11,42 @@ let brightLogin = null;
 let canvasLogin = null;
 let googleLogin = null;
 
-let _brightCourses = [];
-let _canvasCourses = [];
+let _semester = ""
 
-//TODO store brightspace courses
+let _courses = [];
 
-//TODO store canvas courses
+
+let semesters = [
+      {
+         name: "Winter 2019",
+         startDate: new Date('January 5, 2019 0:00:00'),
+         endDate: new Date('April 18, 2019 0:00:00')
+      },
+   
+      {
+         name:"Spring 2019",
+         startDate: new Date('April 22, 2019 0:00:00'),
+         endDate: new Date('August 1, 2019 0:00:00')
+      },
+   
+      {
+         name:"Summer 2019",
+         startDate: new Date('July 29, 2019 0:00:00'),
+         endDate: new Date('September 19, 2019 0:00:00')
+      },
+     
+      {
+         name:"Fall 2019",
+         startDate: new Date('September 16, 2019 0:00:00'),
+         endDate: new Date('December 27, 2019 0:00:00')
+      },
+   
+      {
+         name:"Winter 2020",
+         startDate: new Date('January 8, 2020 0:00:00'),
+         endDate: new Date('April 16, 2020 0:00:00')
+      },
+]
 
 // grab assignments
 
@@ -29,11 +58,11 @@ window.onload = function() {
    init();
 
    brightBtn.addEventListener('click', () => {
-      loginBrightspace();
+      chrome.tabs.create({url: 'https://byui.brightspace.com/d2l/home', active: true});
    });
 
    canvasBtn.addEventListener('click', () => {
-      loginCanvas();
+      chrome.tabs.create({url: 'https://byui.instructure.com/', active: true});
    });
 
    googleBtn.addEventListener('click', () => {
@@ -42,9 +71,11 @@ window.onload = function() {
       });
    });
 
-   syncBtn.addEventListener('click', () => {
-      getEnrollment();
-      getCanvasEnrollment();
+   syncBtn.addEventListener('click', async () => {
+      getBrightspaceEnrollment();
+      await getCanvasEnrollment();
+      console.log(_courses);
+
    })
 };
 
@@ -59,6 +90,8 @@ async function init(){
    canvasLogin = document.querySelector('.canvasLogin');
    googleLogin = document.querySelector('.googleLogin');
 
+   getCurrentSemester();
+
    await checkLoginStatus();
    renderWelcome();
 };
@@ -66,6 +99,10 @@ async function init(){
 function renderWelcome(){
    let username = _userBrightspace || _userCanvas || _userGoogle;
    let welcome = document.querySelector('.welcome');
+   let heading = document.querySelector('.heading');
+   let semester = document.createElement('h5');
+   semester.innerHTML = _semester.name;
+   heading.appendChild(semester);
    if(username != null){
       welcome.innerHTML = `Welcome, ${username}!`;
    }else{
@@ -92,36 +129,26 @@ function getJSON(url){
 function getText(url){
    return fetch(url)
       .then(response =>{
-         return response.text();
+         if(response.ok){
+            return response.text();
+
+         }
+         else{
+            throw Error(response.statusText);
+         }
       })
       .catch(error => {
          console.log(error);
       });
 }
 
-async function loginBrightspace(){
-   //FIXME Not woking properly when logged off.
-   try{ 
-      chrome.tabs.create({url: 'https://byui.brightspace.com/d2l/home', active: true});
-   }catch(error){
-      console.log("Did not return user. Not logged in, maybe?");
-      console.log("Error: " + error);      
-   }
-}
-
-async function loginCanvas(){
-   try {
-      let url = "https://byui.instructure.com/api/v1/users/self";
-      _userCanvas = JSON.parse((await getText(url)).split(";")[1]).name;
-      // console.log(_userCanvas);
-      showLoggedIn(canvasLogin, canvasBtn);
-      checkLoginStatus();
-   } catch (error) {
-      console.log("Did not return Canvas user. Not logged in, maybe?");
-      console.log("Error: " + error);
-      chrome.tabs.create({url: 'https://byui.instructure.com/', active: false});
-      showNotLoggedIn(canvasLogin,canvasBtn);
-      checkLoginStatus();
+function getCurrentSemester(){
+   let current = new Date();
+   for (let semester of semesters) {
+      // console.log(`Semester Start: ${semester.startDate.toISOString()} Semester End: ${semester.endDate.toISOString()}`);
+      if(current > semester.startDate && current < semester.endDate){
+         _semester = semester;
+      }
    }
 }
 
@@ -139,7 +166,7 @@ async function checkLoginStatus(){
    if(_userBrightspace == null){
       try{
          let user =  await getJSON("https://byui.brightspace.com/d2l/api/lp/1.9/users/whoami");
-         //console.log(user);
+         // console.log(user);
          _userBrightspace = user.FirstName;
          showLoggedIn(brightLogin, brightBtn);
       }catch(error){
@@ -149,20 +176,21 @@ async function checkLoginStatus(){
    }
    
    if(_userCanvas == null){
-      let response =  await getText("https://byui.instructure.com/api/v1/users/self");
-      let name = JSON.parse(response.split(";")[1]).name;
-      _userCanvas = name;
-      if(_userCanvas != null){
-         showLoggedIn(canvasLogin, canvasBtn);
+      try{
+            let response =  await getText("https://byui.instructure.com/api/v1/users/self");
+            let name = JSON.parse(response.split(";")[1]).name;
+            _userCanvas = name;
+         if(_userCanvas != null){
+            showLoggedIn(canvasLogin, canvasBtn);
+         }else{
+            showNotLoggedIn(canvasLogin, canvasBtn);
+         }
       }
-   }else{
-      showLoggedIn(canvasLogin, canvasBtn);
+      catch(Error){
+         showNotLoggedIn(canvasLogin,canvasBtn);
+         console.log(Error);
+      }
    }
-   
-   let test = false;
-   if(_userBrightspace && _userCanvas){
-      test = true;
-   };
 
    if(_userBrightspace != null && _userCanvas != null){
       syncBtn.disabled = false;
@@ -171,32 +199,22 @@ async function checkLoginStatus(){
    }
 }
 
-async function getEnrollment(){
-   let startDate = new Date();
-   let endDate = new Date();
-   startDate.setMonth(startDate.getMonth() - 3);
-   endDate.setMonth(endDate.getMonth() + 3);
-   
-   let testDate = new Date()
-
-   console.log(`Start Date: ${startDate}, End Date: ${endDate}`);
-   console.log(`https://byui.brightspace.com/d2l/api/lp/1.9/enrollments/myenrollments/?orgUnitTypeId=3&sortBy=-EndDate&startDateTime=${testDate.toISOString()}&endDateTime=${endDate.toISOString()}`)
-   
-   await getJSON(`https://byui.brightspace.com/d2l/api/lp/1.9/enrollments/myenrollments/?orgUnitTypeId=3&sortBy=-EndDate&startDateTime=${testDate.toISOString()}&endDateTime=${endDate.toISOString}`)
+async function getBrightspaceEnrollment(){   
+   await getJSON(`https://byui.brightspace.com/d2l/api/lp/1.9/enrollments/myenrollments/?orgUnitTypeId=3&sortBy=-EndDate`)
    .then( results =>{
-      let array = [];
       for(let item of results.Items){
          if(item.Access.StartDate != null || item.Access.EndDate != null){
             let temp = {};
-            temp.name = item.OrgUnit.Name;
-            temp.id = item.OrgUnit.Id;
-            array.push(item);
+            temp.startDate = new Date(item.Access.StartDate);
+            temp.endDate = new Date(item.Access.EndDate);
+            if(temp.startDate.getTime() >= _semester.startDate.getTime() && temp.endDate.getTime() <= _semester.endDate.getTime()){
+               temp.name = item.OrgUnit.Name;
+               temp.id = item.OrgUnit.Id;      
+               temp.source = "brightspace";
+               _courses.push(temp);
+            }
          }
       }
-      //TODO Store only the relevant data from the query into an array of objects;
-      console.log(array);
-      
-      
       console.log(results);
    })
    .catch(error => {
@@ -204,15 +222,37 @@ async function getEnrollment(){
    })
    
 }
+
 async function getCanvasEnrollment(){
-   
-   await getText("https://byui.instructure.com/api/v1/users/self/enrollments")
+   await getText('https://byui.instructure.com/api/v1/users/self/courses')
    .then( results => {
-      console.log(JSON.parse(results.split(";")[1]));
-     
+      let array = [];
+      let courses = JSON.parse(results.split(";")[1]);
+      for(let item of courses){
+         let temp = {};
+         temp.startDate = new Date(item.start_at);
+         if(temp.startDate.getTime() >= _semester.startDate.getTime() && temp.startDate.getTime() <= _semester.endDate.getTime()){
+            temp.name = item.name;
+            temp.id = item.id;
+            temp.source = "canvas";
+            _courses.push(temp);
+         }
+      }
+      console.log(courses);
    })
    .catch(error => {
       console.log(error);
    })
 
+}
+
+async function getCourseInfo(course_id){
+   await getText(`https://byui.brightspace.com/d2l/api/lp/1.9/enrollments/myenrollments/${course_id}`)
+   .then(response => {
+      
+   })
+   .catch(error => {
+      console.log(error);
+   })
+   
 }
